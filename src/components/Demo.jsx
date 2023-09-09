75c32117b173c68f8b972a098fe2cbae1d1ae90d
Hardik Prajapati <prajapatih143145@gmail.com>
2023-09-10 01:31:54 +0530

    10/09/2023⠀⎦⎡⠀01:31⠀⎦⎢⠀Sunday

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets';
+import {useLazyGetSummaryQuery} from '../services/article';
+
+
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+
+    if (articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage);
+    }
+  },[])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //fetch Summary
+    const {data} = await getSummary({articleUrl: article.url});
+
+    if(data?.summary){
+      const newArticle = { ...article, summary: data.summary };
+
+      const updateAllArticles = [newArticle, ...allArticles ];
+
+      SetArticle(newArticle);
+      setAllArticles(updateAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updateAllArticles));
+
+      console.log(newArticle);
+    }
+    
+  }
+
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      {/* search */}
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}>
+
+          <img src={linkIcon} alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5" />
+
+          <input type="url" placeholder="Enter a URL" value={article.url} 
+          onChange={(e) => SetArticle({ ...article, url: e.target.value })} required className="url_input peer" />
+
+          <button type="submit" className="submit_btn peer-focus:border-gray-700
+          peer-focus:text-gray-700">
+            ↵
+          </button>
+        </form>
+
+        {/* Browser URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              <div className="copy_btn">
+                <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+                </div> 
+            </div>
+          ))}
+        </div>
+
+      </div>
+
+      {/* Display Results */}
+
+    </section>
   )
 }
 
